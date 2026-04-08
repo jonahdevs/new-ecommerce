b614535bdd8001c0dc5bdf508d488226e3aa9fe9
Jonah Wakahiu <jonah.wakahiumaina@gmail.com>
2026-04-08 20:40:22 +0300

    modified the product details page

--- resources/views/pages/product-details/partials/_specification.blade.php
+++ resources/views/pages/product-details/partials/_specification.blade.php
@@ -1,9 +1,9 @@
 <div wire:cloak wire:show="selectedTab == 'specification'">
     @if (!empty($product->technical_specification))
-        <div class="prose prose-sm max-w-none dark:prose-invert">
+        <div class="text-sm text-zinc-500 tracking-wider leading-6">
             {!! $product->technical_specification !!}
         </div>
     @else
-        <p class="text-sm text-zinc-400">No specifications available for this product.</p>
+        <p class="text-sm text-zinc-500">No specifications available for this product.</p>
     @endif
 </div>
